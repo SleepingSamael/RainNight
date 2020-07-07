01fe0d73ecdc1fe56718238a04a835525b7b81ea
SleepingSamael <737025446@qq.com>
2020-07-07 23:52:47 +0800

    Add files via upload

--- rainy-night/js/index.js
+++ rainy-night/js/index.js
@@ -1,6 +1,7 @@
 $("#switch").on('click', function () {
 		if (!$("body").hasClass("stop-rain"))  {
 			document.getElementById('rain').hidden = true
+			$("#text_rain").html("- begin to rain -");
 			$("#switch").addClass("switched");
 			$("body").addClass("stop-rain");
 			slowClock()
@@ -8,6 +9,7 @@ $("#switch").on('click', function () {
 		else {
 			$("body").removeClass("stop-rain");
 			document.getElementById('rain').hidden = false
+			$("#text_rain").html("- stop the rain -")
 			$("#switch").removeClass("switched");
 			fastClock()
 			if($("body").hasClass("off-cloud")){
